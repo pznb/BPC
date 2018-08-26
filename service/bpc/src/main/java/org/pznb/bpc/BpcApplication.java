632ca7445f143ead1a1c9f543ce32304c9c733e3
Your Name <you@example.com>
2018-08-26 15:07:42 +0800

    bpc初始化

---
+++ service/bpc/src/main/java/org/pznb/bpc/BpcApplication.java
@@ -0,0 +1,12 @@
+package org.pznb.bpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpcApplication.class, args);
+	}
+}
